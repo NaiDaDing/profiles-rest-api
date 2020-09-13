95eda8232ab1157f6dbe5f2135d871bad956bf63
Ding <qq122618071@gmail.com>
2020-09-14 01:46:35 +0800

    Added user profile feed API

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -4,3 +4,4 @@ from profiles_api import models # Import models.py from our app
 
 # Register your models here.
 admin.site.register(models.UserProfile) # After register, this model will show up on admin interface
+admin.site.register(models.ProfileFeedItem)
